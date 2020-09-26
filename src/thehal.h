1e5bb071a1f5de0837db37023ce2f125e1de393b
J-Rios <jrios.github@gmail.com>
2020-09-26 23:35:58 +0200

    Created "Digital Out Controller" component.

---
+++ src/thehal.h
@@ -0,0 +1,48 @@
+
+/**
+ * @file    thehal.h
+ * @author  Jose Miguel Rios Rubio <jrios.github@gmail.com>
+ * @date    26-09-2020
+ * @version 1.0.0
+ *
+ * @section DESCRIPTION
+ *
+ * TheHal Project Configurations File.
+ *
+ * @section LICENSE
+ *
+ * Copyright (c) 2020 Jose Miguel Rios Rubio. All right reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/*****************************************************************************/
+
+/* Include Guards */
+#ifndef THE_HAL_H_
+#define THE_HAL_H_
+
+/*****************************************************************************/
+
+/* TheHal General Configurations */
+
+/* Enable/Disable GPIO Components */
+#define THE_HAL_COMPONENT_DIGITAL_OUT 1
+
+
+
+/*****************************************************************************/
+
+#endif // THE_HAL_H_
