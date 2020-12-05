0189edd1efd2b89f24c3abf1371c1a2266a6ba98
J-Rios <jrios.github@gmail.com>
2020-12-05 14:59:04 +0100

    Digital Out Controller Arduino example update.

--- src/components/digital_out_controller/examples/arduino/digital_out_controller/digital_out_controller.ino
+++ src/components/digital_out_controller/examples/arduino/digital_out_controller/digital_out_controller.ino
@@ -7,13 +7,14 @@
 
 /*****************************************************************************/
 
-DigitalOut MyLed(GPIO_LED);
+DigitalOut MyLed;
 
 /*****************************************************************************/
 
 void setup()
 {
-    MyLed.setup();
+    // Initialize GPIO_LED pin as digital output and LOW level
+    MyLed.setup(GPIO_LED, LOW);
 }
 
 void loop()
@@ -21,9 +22,9 @@ void loop()
     // Heart Beat
     for(uint8_t i = 0; i < 2; i++)
     {
-        MyLed.set_high();
+        MyLed.set_high(); // Same as: digitalWrite(GPIO_LED, HIGH);
         delay(120);
-        MyLed.set_low();
+        MyLed.set_low();  // Same as: digitalWrite(GPIO_LED, LOW);
         delay(120);
     }
     delay(500);
